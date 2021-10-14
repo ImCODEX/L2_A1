6ea8c08facdc25a08c419f3984f1467d4a7eb23c
Razvan Postescu <razvanpostescu@gmail.com>
2021-10-14 15:19:22 +0300

    Added javadoc

--- src/Execute/Methods.java
+++ src/Execute/Methods.java
@@ -4,10 +4,12 @@ public class Methods {
 
     int[] array_of_notes = new int[]{10, 14, 23, 25, 38, 41, 43, 47, 56, 59, 63, 66, 68, 74, 78, 86, 89, 90, 92, 95, 98};
 
-    //in: int[]; out: int[]
-    //Rounds up each element (i) in a give array
-    //if the difference between i and the next multiple
-    //of 5 <= 2
+    /**
+     * Rounds up each element (i) in a give array
+     * if the difference between i and the next multiple
+     * @param array_of_notes: array of notes
+     * @return: array of rounded up notes
+     */
     public int[] round_up(int[] array_of_notes) {
         int[] array_of_notes1 = array_of_notes.clone();
         for (int i = 0; i < array_of_notes1.length; i++) {
@@ -19,8 +21,11 @@ public class Methods {
         return array_of_notes1;
     }
 
-    //in: int[]; out: int[]
-    //Determines if a note is not sufficient (<40)
+    /**
+     * Determines if a note is not sufficient (<40)
+     * @param array_of_notes: array of notes
+     * @return: array of insufficient notes
+     */
     public int[] not_sufficient(int[] array_of_notes) {
         int[] not_sufficient_notes = new int[array_of_notes.length];
         int j = 0;
@@ -33,8 +38,11 @@ public class Methods {
         return not_sufficient_notes;
     }
 
-    //in: int[]; out: int
-    //Computes average of notes
+    /**
+     * Computes average of notes
+     * @param array_of_notes: array of notes
+     * @return: average of notes
+     */
     public int average(int[] array_of_notes){
         int avg=0;
         for (int array_of_note : array_of_notes) {
@@ -44,8 +52,11 @@ public class Methods {
         return avg;
     }
 
-    //in: int[]; out: int
-    //Determines the maximum note that will be rounded up
+    /**
+     * Determines the maximum note that will be rounded up
+     * @param array_of_notes: array of notes
+     * @return: max note
+     */
     public int max_rounded_up(int[] array_of_notes) {
         int max = 0;
         for (int i = 0; i < array_of_notes.length; i++) {
