4549667bad96dc6e914c4785564a8c4c0792583e
rpostescu_01 <53984429+ImCODEX@users.noreply.github.com>
2022-03-10 16:25:58 +0200

    Cleaned code

--- src/Execute/Methods.java
+++ src/Execute/Methods.java
@@ -6,66 +6,65 @@ public class Methods {
     /**
      * Rounds up each element (i) in a give array
      * if the difference between i and the next multiple
-     * @param array_of_grades: array of grades
+     * @param arrayOfGrades: array of grades
      * @return: array of rounded up grades
      */
-    public int[] round_up(int[] array_of_grades) {
-        int[] array_of_grades1 = array_of_grades.clone();
-        for (int i = 0; i < array_of_grades1.length; i++) {
-            if ((array_of_grades1[i] + 1) % 5 == 0)
-                array_of_grades1[i] = array_of_grades1[i] + 1;
-            else if ((array_of_grades1[i] + 2) % 5 == 0)
-                array_of_grades1[i] = array_of_grades1[i] + 2;
+    public int[] roundUp(int[] arrayOfGrades) {
+        int[] arrayOfGrades1 = arrayOfGrades.clone();
+        for (int i = 0; i < arrayOfGrades1.length; i++) {
+            if ((arrayOfGrades1[i] + 1) % 5 == 0)
+                arrayOfGrades1[i] = arrayOfGrades1[i] + 1;
+            else if ((arrayOfGrades1[i] + 2) % 5 == 0)
+                arrayOfGrades1[i] = arrayOfGrades1[i] + 2;
         }
-        return array_of_grades1;
+        return arrayOfGrades1;
     }
 
     /**
      * Determines if a note is not sufficient (<40)
-     * @param array_of_grades: array of grades
+     * @param arrayOfGrades: array of grades
      * @return: array of insufficient grades
      */
-    public int[] not_sufficient(int[] array_of_grades) {
-        int[] not_sufficient_grades = new int[array_of_grades.length];
+    public int[] notSufficient(int[] arrayOfGrades) {
+        int[] notSufficientGrades = new int[arrayOfGrades.length];
         int j = 0;
-        for (int array_of_note : array_of_grades) {
-            if (array_of_note < 40) {
-                not_sufficient_grades[j] = array_of_note;
+        for (int arrayOfNote : arrayOfGrades) {
+            if (arrayOfNote < 40) {
+                notSufficientGrades[j] = arrayOfNote;
                 j++;
             }
         }
 
-        int[] not_sufficient_grades_filtered = new int[j];
-        System.arraycopy(not_sufficient_grades, 0, not_sufficient_grades_filtered, 0, j);
+        int[] notSufficientGradesFiltered = new int[j];
+        System.arraycopy(notSufficientGrades, 0, notSufficientGradesFiltered, 0, j);
 
-
-        return not_sufficient_grades_filtered;
+        return notSufficientGradesFiltered;
     }
 
     /**
      * Computes average of grades
-     * @param array_of_grades: array of grades
+     * @param arrayOfGrades: array of grades
      * @return: average of grades
      */
-    public int average(int[] array_of_grades){
-        int avg=0;
-        for (int array_of_note : array_of_grades) {
-            avg += array_of_note;
+    public int average(int[] arrayOfGrades){
+        int average=0;
+        for (int arrayOfNote : arrayOfGrades) {
+            average += arrayOfNote;
         }
-        avg /= array_of_grades.length;
-        return avg;
+        average /= arrayOfGrades.length;
+        return average;
     }
 
     /**
      * Determines the maximum rounded up grade
-     * @param array_of_grades: array of grades
+     * @param arrayOfGrades: array of grades
      * @return: max grade
      */
-    public int max_rounded_up(int[] array_of_grades) {
+    public int maxRoundedUp(int[] arrayOfGrades) {
         int max = 0;
-        for (int array_of_grade : array_of_grades)
-            if (array_of_grade % 5 >= 3 && array_of_grade > max)
-                max = array_of_grade + (5 - array_of_grade % 5);
+        for (int arrayOfGrade : arrayOfGrades)
+            if (arrayOfGrade % 5 >= 3 && arrayOfGrade > max)
+                max = arrayOfGrade + (5 - arrayOfGrade % 5);
 
         return max;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Methods m = new Methods();
 
-        int[] array_of_grades = new int[]{10, 14, 23, 25, 38, 41, 43, 47, 56, 30, 63, 66, 68, 74, 78, 86, 89, 90, 92, 95, 98};
+        int[] arrayOfGrades = new int[]{10, 14, 23, 25, 38, 41, 43, 47, 56, 30, 63, 66, 68, 74, 78, 86, 89, 90, 92, 95, 98};
 
         //Test for round_up(int[]) method
-        System.out.println(java.util.Arrays.toString(m.round_up(array_of_grades)));
+        System.out.println(java.util.Arrays.toString(m.roundUp(arrayOfGrades)));
 
         //Test for not_sufficient(int[]) method
-        System.out.println(java.util.Arrays.toString(m.not_sufficient(array_of_grades)));
+        System.out.println(java.util.Arrays.toString(m.notSufficient(arrayOfGrades)));
 
         //Test for average(int[]) method
-        System.out.println(m.average(array_of_grades));
+        System.out.println(m.average(arrayOfGrades));
 
         //Test for max_rounded_up(int[]) method
-        System.out.println(m.max_rounded_up(array_of_grades));
+        System.out.println(m.maxRoundedUp(arrayOfGrades));
 
     }
 }
